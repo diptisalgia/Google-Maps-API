a6cfd77fba2024f9a50dff4991a914feb039ef8b
diptisalgia <diptisalgia@gmail.com>
2017-02-05 18:02:07 +0530

    Added ButtonDropdown

--- JS/Map.js
+++ JS/Map.js
@@ -1,44 +1,75 @@
 var myMaps = {
 
 	map : null,
-
 	marker : null,
+	curPosition : null,
+
 
 	Init : function() {
 		var me = myMaps;
 		me.InitializeMaps(22.7196, 75.8577);
 		me.BindEvents();
+		me.GetLocation();
 
 	},
 
 	BindEvents : function() {
 		var me = myMaps;
 		$("#search_button").off("click");
-		$("#search_button").on("click",
-				 function() {
+		$("#search_button").on(
+				"click",
+				function() {
 					me.showDirections($("#source").val().trim(), $(
 							"#destination").val().trim());
 
 				});
 
-		$("#getCurrentLoc").off("click");
-		$("#getCurrentLoc").on("click",function(){
-			me.GetLocation();
+		$(".searchoptions").off("click");
+		$(".searchoptions").on("click", function() {
+			console.log("clicking on any options");
+			var id = this.id;
+			if (id == "getCurrentLoc") {
+				me.GetCurrentLocationOnMap();
+			} else if (id == "currentLocInSource") {
+				me.GetCurrentLocationAddress();
+			}
+
 		});
 
-		$("#currentLocInSource").off("click");
-		$("#currentLocInSource").on("click",function(){
-
-		});
 	},
 
 	GetLocation : function() {
 		var me = myMaps;
-console.log("HI");
 		var geolocation = navigator.geolocation;
 		geolocation.getCurrentPosition(function(position) {
-			me.InitializeMaps(position.coords.latitude,
-					position.coords.longitude);
+			var pos = {
+				lat : position.coords.latitude,
+				lng : position.coords.longitude
+			};
+			me.curPosition = pos;
+
+		});
+	},
+
+	GetCurrentLocationOnMap : function() {
+		var me = myMaps;
+		me.marker.setMap(null);
+		me.PlaceMarker(me.curPosition.lat, me.curPosition.lng);
+
+	},
+
+	GetCurrentLocationAddress : function() {
+		var me = myMaps;
+		console.log("Welcome");
+
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({
+			'location' : me.curPosition
+		}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				console.log(results[1].formatted_address);
+				$("#source").val(results[1].formatted_address);
+			}
 
 		});
 	},
@@ -74,13 +105,13 @@ console.log("HI");
 		var me = myMaps;
 		console.log(Latitude, Longitude);
 		var mapOptions = {
-			center : new google.maps.LatLng(Latitude,Longitude),
+			//	center : new google.maps.LatLng(Latitude,Longitude),
 			zoom : 8
 		};
 
 		me.map = new google.maps.Map($("#map")[0], mapOptions);
-		me.PlaceMarker(mapOptions, me.map);
-
+		//me.PlaceMarker(mapOptions.center);
+		me.PlaceMarker(Latitude, Longitude);
 		new google.maps.places.Autocomplete($("#source")[0]).bindTo('bounds',
 				me.map);
 		new google.maps.places.Autocomplete($("#destination")[0]).bindTo(
@@ -88,14 +119,14 @@ console.log("HI");
 
 	},
 
-	PlaceMarker : function(mapOptions, map) {
+	PlaceMarker : function(lat, lng) {
 		var me = myMaps;
-
 		//The google.maps.Marker constructor takes a single Marker options object literal, specifying the initial properties of the marker
 		me.marker = new google.maps.Marker({
-			position : mapOptions.center,
-			map : map
+			position : new google.maps.LatLng(lat, lng),
+			map : me.map
 		});
+		me.map.setCenter(me.marker.getPosition());
 	}
 }
 
--- JS/myMaps.js
+++ JS/myMaps.js
@@ -80,3 +80,108 @@ var myMaps = {
 $(function() {
 	myMaps.Init();
 });
+/*var myMaps = {
+
+	map : null,
+
+	marker : null,
+
+	Init : function() {
+		var me = myMaps;
+		me.InitializeMaps(22.7196, 75.8577);
+		me.BindEvents();
+
+	},
+
+	BindEvents : function() {
+		var me = myMaps;
+		$("#search_button").off("click");
+		$("#search_button").on("click",
+				 function() {
+					me.showDirections($("#source").val().trim(), $(
+							"#destination").val().trim());
+
+				});
+
+		$("#getCurrentLoc").off("click");
+		$("#getCurrentLoc").on("click",function(){
+			me.GetLocation();
+		});
+
+		$("#currentLocInSource").off("click");
+		$("#currentLocInSource").on("click",function(){
+
+		});
+	},
+
+	GetLocation : function() {
+		var me = myMaps;
+console.log("HI");
+		var geolocation = navigator.geolocation;
+		geolocation.getCurrentPosition(function(position) {
+			me.InitializeMaps(position.coords.latitude,
+					position.coords.longitude);
+
+		});
+	},
+
+	showDirections : function(source, destination) {
+		var me = myMaps;
+
+		var directionsService = new google.maps.DirectionsService;
+		var directionsDisplay = new google.maps.DirectionsRenderer();
+
+		var directionsRequest = {
+			origin : source,
+			destination : destination,
+			travelMode : google.maps.DirectionsTravelMode.DRIVING,
+			unitSystem : google.maps.UnitSystem.METRIC
+		};
+
+		directionsService.route(directionsRequest, function(response, status) {
+			if (status == google.maps.DirectionsStatus.OK) {
+				me.marker.setMap(null);
+				directionsDisplay.setMap(me.map);
+				directionsDisplay.setDirections(response);
+			} else
+				alert("Unable to retrieve your route<br />");
+		});
+
+		$("div#rightPanel").empty();
+		directionsDisplay.setPanel($("#rightPanel")[0]);
+
+	},
+
+	InitializeMaps : function(Latitude, Longitude) {
+		var me = myMaps;
+		console.log(Latitude, Longitude);
+		var mapOptions = {
+			center : new google.maps.LatLng(Latitude,Longitude),
+			zoom : 8
+		};
+
+		me.map = new google.maps.Map($("#map")[0], mapOptions);
+		me.PlaceMarker(mapOptions, me.map);
+
+		new google.maps.places.Autocomplete($("#source")[0]).bindTo('bounds',
+				me.map);
+		new google.maps.places.Autocomplete($("#destination")[0]).bindTo(
+				'bounds', me.map);
+
+	},
+
+	PlaceMarker : function(mapOptions, map) {
+		var me = myMaps;
+
+		//The google.maps.Marker constructor takes a single Marker options object literal, specifying the initial properties of the marker
+		me.marker = new google.maps.Marker({
+			position : mapOptions.center,
+			map : map
+		});
+	}
+}
+
+$(function() {
+	myMaps.Init();
+});
+*/
